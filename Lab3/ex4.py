c80c9c61e2443306c832a1c66c4ecad295c45df2
AlexandraUrsu5 <79244891+AlexandraUrsu5@users.noreply.github.com>
2022-10-24 13:01:47 +0300

    Add files via upload

--- Lab3/ex4.py
+++ Lab3/ex4.py
@@ -1,6 +1,6 @@
 def build_xml_element(tag, content, **parameters):
-    string_params = " ".join([p[0]+" = \"" + p[1] + "\"" if type(p[1]) == str else p[0]+" = " + str(p[1])
-                                        for p in parameters.items()])
+    string_params = " ".join([p[0]+" = \"" + p[1] + "\"" 
+                for p in parameters.items()])
     return "<" + tag + " " + string_params + ">"+content+"</"+tag+">"    
    
 def main():
