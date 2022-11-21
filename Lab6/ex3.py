43368bceb1677cbfe080052cb2b8dac2e824cf7a
AlexandraUrsu5 <79244891+AlexandraUrsu5@users.noreply.github.com>
2022-11-21 13:42:25 +0200

    Add files via upload

--- Lab6/ex3.py
+++ Lab6/ex3.py
@@ -4,6 +4,6 @@ def function3(txt, list):
     return [el for el in txt if any([re.search(r, el) for r in list])]                                             
                                                   
 def main():
-    print(function3(['Mama', 'face', 'placinte.'], ["\d", "\d\d", "\wte"])) 
+    print(function3(['Mama', 'face','1', 'placinte.'], ["\d", "\wce", "\wte","\d", "\d{2}"])) 
       
 main()                                           
--- Lab6/ex6.py
+++ Lab6/ex6.py
@@ -1,8 +1,10 @@
 import re
 
+
 def censures(s):
-    low_s = s.group(0)
-    if not (low_s[0] in "aeiouAEIOU" and low_s[-1] in "aeiouAEIOU"):
+    word = s.group(0)
+    vowels = "aeiou"
+    if re.match("[aeiou]",word):
         return s.group(0)
     return "".join([ch if idx % 2 == 0 else '*' for idx, ch in enumerate(s.group(0))])
 
